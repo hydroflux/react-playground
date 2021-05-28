1ca5b1e2b1093b59622286e46fd8ef77ae83f9cc
Jack Hubert <jphubert93@gmail.com>
2021-05-28 12:01:30 -0600

    created initial framework for dinosaur cards & dinosaur section

---
+++ src/containers/DinosaurContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function DinosaurContainer() {
+
+    const displayDinosaurCards = () => {
+        // some code
+    }
+
+    return (
+        <section className="dinosaurs-section">
+            {displayDinosaurCards}
+        </section>
+    )
+}
